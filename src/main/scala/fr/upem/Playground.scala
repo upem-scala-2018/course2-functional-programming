e44a7ad3fbd1e5db1f6cc58f5a6eff0e5ea12348
Ilja <ilja.kempf@gmail.com>
2018-10-15 10:47:57 +0200

    Remove refined in playground

--- src/main/scala/fr/upem/Playground.scala
+++ src/main/scala/fr/upem/Playground.scala
@@ -6,14 +6,5 @@ import eu.timepit.refined._
 
 object Playground extends App {
 
-  type PositiveInt = Refined[Int, Positive]
-
-  def fib(n : PositiveInt): PositiveInt = {
-    if(n.value == 1 || n.value == 2) refineMV(1)
-    else fib(refineV(n.value - 1))
-  }
-
-  fib(refineMV(5))
 
 }
-
