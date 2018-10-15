23c30217231c5f75939cb97b7cc232aab6cf8f8f
Ilja <ilja.kempf@gmail.com>
2018-10-15 15:46:58 +0200

    ADT solution

--- src/main/scala/fr/upem/course2/_5_ADT.scala
+++ src/main/scala/fr/upem/course2/_5_ADT.scala
@@ -14,14 +14,13 @@ object _5_ADT {
   case object Blue
 
   // 5.2 Créer un type produit pour un rectangle avec son hauteur et largeur
-  // case class Rectangle ???
 
   // 5.3 Créer un type produit pour un cercle avec son rayon
-  // case class Circle ???
 
   // 5.4 Combiner les deux types produits précedants dans un ADT
   sealed trait Shape
-  // ...
+  case class Rectangle(height: Int, width: Int) extends Shape
+  case class Circle(radius: Int) extends Shape
 
 
   // 5.5 L'ADT suivant décrit un simple DSL
@@ -33,17 +32,14 @@ object _5_ADT {
   // - expression booléenne
   // - expression si vrai (le if)
   // - expression si faux (le else)
-  sealed trait Expr
-  case object Exit extends Expr
-  case class EInt(a: Int) extends Expr
-  case class EBool(a: Int) extends Expr
-  case class EString(a: String) extends Expr
-  //case class EAdd(???) extends Expr
-  //case class ESucc(???) extends Expr
-  //case class EPrtLn(???) extends Expr
-  //case class ECond(???) extends Expr
-
-
-
+  sealed trait Expr[Int]
+  case object Exit extends Expr[Nothing]
+  case class EInt(a: Int) extends Expr[Int]
+  case class EBool(a: Int) extends Expr[Boolean]
+  case class EString(a: String) extends Expr[String]
+  case class EAdd(lhs: Expr[Int], rhs: Expr[Int]) extends Expr[Int]
+  case class ESucc(n: Expr[Int]) extends Expr[Int]
+  case class EPrtLn(s: Expr[String]) extends Expr[Unit]
+  case class ECond[A](b: Expr[Boolean], ifTrue: Expr[A], ifElse: Expr[A]) extends Expr[A]
 
 }
